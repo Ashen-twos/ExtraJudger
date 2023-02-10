425ad82347e590bf3dbbd92f2daf8510367b1d08
Ashen-twos <15360421037@163.com>
2023-02-10 22:50:41 +0800

    cp instead of make

--- build.sh
+++ build.sh
@@ -3,12 +3,5 @@
 set -xe
 
 cp -rf include/spdlog /usr/include/
-
-cd src
-git clone https://github.com/gabime/spdlog.git
-cd spdlog && mkdir build && cd build
-cmake .. && make -j 
-
-cd ../../..
 cd binding
 bash build.sh
