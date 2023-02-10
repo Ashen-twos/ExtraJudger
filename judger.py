cd9250ae143f5e901f853f6718600642852280fe
Ashen-twos <15360421037@163.com>
2023-02-11 06:05:45 +0800

    extra_config

--- judger.py
+++ judger.py
@@ -19,12 +19,21 @@ class JudgeServer:
         result = exjudger.judge(src, indentSize, leftBigPara)
         ret = {"pass":result=="success", "info":result}
         return ret
-
+    
+    @classmethod
+    def test(cls, src, config):
+        test_cases = []
+        if "format" in config:
+            indentSize = config["format"]["indentSize"]
+            leftBigPara = config["format"]["leftBigPara"]
+            result = exjudger.judge(src, indentSize, leftBigPara)
+            test_cases.append({"name":"format", "pass":result=="success", "info":result})
+        return test_cases
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>', methods=["POST"])
 def server(path):
-    if path in ("exjudge", "ping"):
+    if path in ("exjudge", "ping", "test"):
         try:
             data = request.json
         except Exception:
