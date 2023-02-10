9b2e7ea7f551b9a16eda44bc7868e569b4c3266c
Ashen-twos <15360421037@163.com>
2023-02-10 20:45:05 +0800

    flask test

--- judger.py
+++ judger.py
@@ -1,11 +1,38 @@
+import binding.exjudger as exjudger
+import hashlib
+import json
 import os
+import shutil
+import uuid
+from flask import Flask, request, Response
 
-file = open('1.in')
+app = Flask(__name__)
 
-cnt = 0
+class JudgeServer:
+    @classmethod
+    def ping(cls):
+        data = server_info()
+        data["action"] = "pong"
+        return data
+    @classmethod
+    def exjudge(cls, src, indentSize, leftBigPara):
+        result = exjudger.judge(src, indentSize, leftBigPara)
+        ret = {"pass":result=="success", "info":result}
+        return ret
 
-for line in file:
-    cnt+=1
-    print("line:", cnt)
-    for pos, value in enumerate(line):
-        print(pos,value)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=["POST"])
+def server(path):
+    if path in ("exjudge", "ping"):
+        try:
+            data = request.json
+        except Exception:
+            data = {}
+        ret = {"err": None, "data": getattr(JudgeServer, path)(**data)}
+    else:
+        ret = {"err": "InvalidRequest", "data": "404"}
+    return Response(json.dumps(ret), mimetype='application/json')
+
+if __name__ == "__main__":
+    app.run(debug=True)
